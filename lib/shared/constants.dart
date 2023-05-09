e9c833a3bc5cf3c12d8369bbdcf83d7cca0d956a
Satvik Sharma <satviksharma2607@gmail.com>
2023-05-09 20:53:19 +0530

    update

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants{
+  static String appId="1:877002141778:web:2a65043fa3697e5ff56f8b";
+  static String apiKey="AIzaSyC9dra97ENV8MpCTTKAJAKA4bMXP-8rTaY";
+  static String messagingSenderId="877002141778";
+  static String projectId="chatappflutter-57908";
+}
